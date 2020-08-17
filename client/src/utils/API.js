c1556439777d63f8af5ce6a6964d879874873a2d
eddyangang <eddyyang@berkeley.edu>
2020-08-17 02:35:49 -0700

    Create components for home/search page. Need to link to database

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -19,22 +19,3 @@ export default {
     return axios.post("/api/books", bookData);
   }
 };
-
-
-// getting the books from google api
-// getBooks: function(i) {
-//   return axios.get("/api/google", { params: { i: "title:" + i } });
-// },
-
-// // getting all saved books
-// getSavedBooks: function() {
-//   return axios.get("/api/books");
-// },
-// // deleting books with the given id
-// deleteBook: function(id) {
-//   return axios.delete("/api/books/" + id);
-// },
-// // saves the book to the database
-// saveBook: function(bookData) {
-//   return axios.post("/api/books", bookData);
-// }
